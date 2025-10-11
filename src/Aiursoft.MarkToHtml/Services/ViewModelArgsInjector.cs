e8f49934d92eb5e55721519f8db5a994d57c3be5
Anduin Xue <anduin@aiursoft.com>
2025-10-11 07:02:33 +0000

    Update branding from "Template" to "Markdown to HTML converter" and add localized strings for new branding across all supported languages

--- src/Aiursoft.MarkToHtml/Services/ViewModelArgsInjector.cs
+++ src/Aiursoft.MarkToHtml/Services/ViewModelArgsInjector.cs
@@ -76,13 +76,13 @@ public class ViewModelArgsInjector(
     {
         var preferDarkTheme = context.Request.Cookies[ThemeController.ThemeCookieKey] == true.ToString();
         toInject.PageTitle = localizer[toInject.PageTitle ?? "View"];
-        toInject.AppName = localizer["Template"];
+        toInject.AppName = localizer["Markdown to HTML converter"];
         toInject.Theme = preferDarkTheme ? UiTheme.Dark : UiTheme.Light;
         toInject.SidebarTheme = preferDarkTheme ? UiSidebarTheme.Dark : UiSidebarTheme.Default;
         toInject.Layout = UiLayout.Fluid;
         toInject.FooterMenu = new FooterMenuViewModel
         {
-            AppBrand = new Link { Text = localizer["Template"], Href = "https://gitlab.aiursoft.cn/aiursoft/template" },
+            AppBrand = new Link { Text = localizer["Markdown to HTML converter"], Href = "https://gitlab.aiursoft.cn/aiursoft/marktohtml" },
             Links =
             [
                 new Link { Text = localizer["Home"], Href = "/" },
@@ -153,7 +153,7 @@ public class ViewModelArgsInjector(
         {
             SideLogo = new SideLogoViewModel
             {
-                AppName = localizer["Aiursoft Template"],
+                AppName = localizer["Markdown to HTML converter"],
                 LogoUrl = "/logo.svg",
                 Href = "/"
             },
