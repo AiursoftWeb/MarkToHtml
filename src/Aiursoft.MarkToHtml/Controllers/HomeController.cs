e8c249396282e2069a0ea51b57d33debfb33a2c0
Anduin Xue <anduin@aiursoft.com>
2025-11-11 15:39:56 +0000

    Remove Mermaid feature and related components

--- src/Aiursoft.MarkToHtml/Controllers/HomeController.cs
+++ src/Aiursoft.MarkToHtml/Controllers/HomeController.cs
@@ -42,40 +42,6 @@ public class HomeController(
         return this.StackView(new IndexViewModel());
     }
 
-    [RenderInNavBar(
-        NavGroupName = "Features",
-        NavGroupOrder = 1,
-        CascadedLinksGroupName = "Home",
-        CascadedLinksIcon = "diagram-3",
-        CascadedLinksOrder = 3,
-        LinkText = "Mermaid",
-        LinkOrder = 3
-    )]
-    public IActionResult Mermaid()
-    {
-        if (!User.Identity!.IsAuthenticated && !appSettings.Value.AllowAnonymousUsage)
-        {
-            logger.LogWarning("Anonymous user trying to access the mermaid page. But it is not allowed.");
-            return Challenge();
-        }
-        return this.StackView(new MermaidViewModel());
-    }
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public IActionResult Mermaid(MermaidViewModel model)
-    {
-        if (!ModelState.IsValid)
-        {
-            return this.StackView(model);
-        }
-
-        // For mermaid, we don't need server-side conversion. We wrap input in a container for client-side rendering.
-        var sanitized = $"<pre class=\"mermaid\">{System.Net.WebUtility.HtmlEncode(model.InputMermaid)}</pre>";
-        model.OutputHtml = sanitized;
-        return this.StackView(model);
-    }
-
     [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Index(IndexViewModel model)
