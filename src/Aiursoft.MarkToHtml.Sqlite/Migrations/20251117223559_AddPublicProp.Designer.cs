ada4e16e753e7229ba63aae6603949be4491d5c6
Aimer Neige <aimer.neige.soft@gmail.com>
2025-11-19 14:21:46 +0000

    sqlite migration

--- src/Aiursoft.MarkToHtml.Sqlite/Migrations/20251117223559_AddPublicProp.Designer.cs
+++ src/Aiursoft.MarkToHtml.Sqlite/Migrations/20251117223559_AddPublicProp.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Aiursoft.MarkToHtml.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Aiursoft.MarkToHtml.Sqlite.Migrations
 {
     [DbContext(typeof(SqliteContext))]
-    partial class SqliteContextModelSnapshot : ModelSnapshot
+    [Migration("20251117223559_AddPublicProp")]
+    partial class AddPublicProp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
