1a07dac75b0ad05886421af99ed4772565876636
Anduin Xue <anduin@aiursoft.com>
2026-02-11 11:40:01 +0000

    Bump template.

--- src/Aiursoft.MarkToHtml/Views/Shared/Components/MarketingNavbar/MarketingNavbarViewModel.cs
+++ src/Aiursoft.MarkToHtml/Views/Shared/Components/MarketingNavbar/MarketingNavbarViewModel.cs
@@ -2,5 +2,6 @@ namespace Aiursoft.MarkToHtml.Views.Shared.Components.MarketingNavbar;
 
 public class MarketingNavbarViewModel
 {
-
+    public string ProjectName { get; set; } = "Aiursoft MarkToHtml";
+    public string LogoUrl { get; set; } = "/logo.svg";
 }
